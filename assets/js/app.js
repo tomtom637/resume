57a9cb02ffe537f19a3eb9b3385aee313d567b2e
Thomas Lacombe <tomtom637@gmail.com>
2022-02-08 18:05:29 +0100

    removed the redirect for fluidity

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,24 @@
 import translations from './translations.js';
 
 let translationState = false;
+const originalTranslations = {};
+
 const translationButton = document.getElementById('translation-button');
 translationButton.addEventListener('click', translate);
 
 function translate() {
-  if(!translationState) {
-    for(const property in translations) {
+  if (!translationState) {
+    for (const property in translations) {
+      originalTranslations[property] = document.querySelector(`[data-translation="${property}"]`).innerText;
       document.querySelector(`[data-translation="${property}"]`).innerText = translations[property];
     }
     translationButton.style.backgroundImage = `url('https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Flag_of_the_United_Kingdom.svg/1600px-Flag_of_the_United_Kingdom.svg.png')`;
     translationState = true;
   } else {
-    document.location.href="https://tomtom637.github.io/resume/";
+    for (const property in originalTranslations) {
+      document.querySelector(`[data-translation="${property}"]`).innerText = originalTranslations[property];
+    }
+    translationButton.style.backgroundImage = `url('https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Flag_of_France.svg/1599px-Flag_of_France.svg.png')`;
+    translationState = false;
   }
 }
