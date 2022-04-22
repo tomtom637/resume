0fbf065afd322114706611d670944982411d9ffd
Thomas Lacombe <tomtom637@gmail.com>
2022-04-22 15:07:33 +0200

    edited education section

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,14 +9,14 @@ translationButton.addEventListener('click', translate);
 function translate() {
   if (!translationState) {
     for (const property in translations) {
-      originalTranslations[property] = document.querySelector(`[data-translation="${property}"]`).innerText;
+      originalTranslations[property] = document.querySelector(`[data-translation="${property}"]`).innerHTML;
       document.querySelector(`[data-translation="${property}"]`).innerHTML = translations[property];
     }
     translationButton.style.backgroundImage = `url('https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Flag_of_the_United_Kingdom.svg/1600px-Flag_of_the_United_Kingdom.svg.png')`;
     translationState = true;
   } else {
     for (const property in originalTranslations) {
-      document.querySelector(`[data-translation="${property}"]`).innerText = originalTranslations[property];
+      document.querySelector(`[data-translation="${property}"]`).innerHTML = originalTranslations[property];
     }
     translationButton.style.backgroundImage = `url('https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Flag_of_France.svg/1599px-Flag_of_France.svg.png')`;
     translationState = false;
--- assets/js/translations.js
+++ assets/js/translations.js
@@ -13,7 +13,7 @@ const translations = {
   guitar: 'guitare',
   education: 'Éducation',
   educationSub: 'développeur web full stack (diplome de niveau 5, bac + 2)',
-  educationContent: 'Je viens de terminer cette formation. <a target="_blank" href="http://165.22.192.63:3000/">Le dernier projet du programme Openclassrooms</a> était la conception d\'un réseau social d\'entreprise',
+  educationContent: `<p class="description-parag">Cette formation couvre:</p><ul class="bulleted-list"><li>HTML, css, javascript</li><li>bonnes pratiques</li><li>les bases de données noSQL et SQL</li><li>NodeJS et le framework ExpressJS</li><li>framework Front-end (ReactJS)</li></ul><a class="main-link" target="_blank" href="http://165.22.192.63:3000/">Un aperçu de mon dernier projet consistant à construir un petit réseau social d'entreprise.</a>`,
   experience: 'Expérience',
   experienceSub: 'Design graphique, ventes et responsable d\'Équipe',
   experienceContent: 'Roc Eclerc est une entreprise du funéraire et j\'étais entre autres choses résponsable de la vente des monuments funéraires et de la gravure des différents articles. J\'étais en charge de leur design (création graphique 3d et 2d), de l\'élaboration des plans techniques, des pochoirs, des retouches photos pour portraits en céramique. J\'aidais au management de notre équipe technique.',
